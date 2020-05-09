8994ffea26e527d5eb6144bfaa6fba8f38f83ba2
Zedjones <dojoman19@gmail.com>
2020-05-08 22:53:06 -0400

    Continued working on migrations

--- migrations/2020-05-07-134206_initial/up.sql
+++ migrations/2020-05-07-134206_initial/up.sql
@@ -1,10 +1,21 @@
 -- Your SQL goes here
 CREATE TABLE computations (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     hr_png BLOB NOT NULL,
     trimmed_png BLOB NOT NULL,
     distance_png BLOB NOT NULL,
     pm_png BLOB,
     correctly_clustered INTEGER,
+    incorrectly_clustered INTEGER,
+    accuracy REAL,
+    anomaly INTEGER
+);
 
-)
+CREATE TABLE clusters
+(
+	computation_id INTEGER NOT NULL,
+	cluster_number INTEGER NOT NULL,
+	stars_number INTEGER NOT NULL,
+	CONSTRAINT clusters_pk
+		PRIMARY KEY (computation_id, cluster_number)
+);
