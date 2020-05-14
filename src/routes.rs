d1590764c458b03e4c1ede02a24a91f33718eed6
Zedjones <dojoman19@gmail.com>
2020-05-14 16:43:05 -0400

    Updated worker, changed route JSON temporarily

--- src/routes.rs
+++ src/routes.rs
@@ -5,6 +5,7 @@ use lapin::{BasicProperties, Channel, options::*};
 use futures::{StreamExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
 use web::{Bytes, Query, Data};
+use log::info;
 
 fn default_db_scan() -> bool {
     false
@@ -18,9 +19,10 @@ fn default_cluster_size() -> i32 {
     200
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Settings {
-    data_id: Option<i32>,
+    data_id: Option<Vec<u8>>,
+    filename: Option<String>,
     #[serde(default = "default_db_scan")]
     db_scan: bool,
     #[serde(default = "default_epsilon")]
@@ -35,6 +37,7 @@ async fn save_file(mut payload: Multipart, mut settings: Query<Settings>,
     // iterate over multipart stream
     while let Ok(Some(mut field)) = payload.try_next().await {
         let mut my_vec: Vec<Bytes> = Vec::new();
+        let filename = field.content_disposition().unwrap().get_filename().unwrap().to_string();
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
             let data = chunk.unwrap();
@@ -43,7 +46,9 @@ async fn save_file(mut payload: Multipart, mut settings: Query<Settings>,
         // Currently not using this until we write to db
         let _all_bytes = my_vec.concat();
         // Random placeholder until we implement db
-        settings.0.data_id = Some(12345);
+        settings.0.data_id = Some(_all_bytes);
+        settings.0.filename = Some(filename);
+        info!("{:?}", &settings.0);
         send_chan.basic_publish(
             "",
             "gaia_input",
--- worker/WebWorker.py
+++ worker/WebWorker.py
@@ -4,6 +4,7 @@ import json
 import io
 import time
 import sys
+import array
 
 from dataclasses import dataclass
 from typing import Optional, List
@@ -25,34 +26,47 @@ class GaiaData:
     anomaly: Optional[int]
     actual_cluster_sizes: Optional[List[int]]
 
-def run_gaia(csv, db_scan, epsilon, cluster_size):
-    csv_file = io.StringIO(csv)
+def run_gaia(csv, db_scan, epsilon, cluster_size, filename):
+    csv_file = io.BytesIO(csv)
     df = create_df(csv_file)
     distance_bytes = io.BytesIO()
-    distance_plot(df, csv_file, distance_bytes)
+    distance_plot(df, csv_file, filename, distance_bytes)
     hr_bytes = io.BytesIO()
-    hr_plots(df, csv_file, hr_bytes)
+    hr_plots(df, csv_file, filename, hr_bytes)
     trimmed_df = trim_data(df)
     trimmed_bytes = io.BytesIO()
-    trimmed_hr(trimmed_df, csv_file, trimmed_bytes)
+    trimmed_hr(trimmed_df, csv_file, filename, trimmed_bytes)
     pm_bytes, correctly_clustered, incorrectly_clustered, accuracy = (None, None, None, None)
     anomaly, actual_cluster_sizes = (None, None)
+    pm_bytes = io.BytesIO()
     if db_scan:
         # do DBScan stuff
-        pm_bytes = io.BytesIO()
-        pm_plots(df, trimmed_df, csv_file, cluster_size)
+        pm_plots(df, trimmed_df, csv_file, cluster_size, filename, pm_bytes)
         df_all_temp = source_id(df, int(cluster_size), int(epsilon))
         df_all, labels, n_clusters, n_noise = machine_learning(df, int(cluster_size), int(epsilon))
         anomaly = n_noise
         actual_cluster_sizes = amount(labels, n_clusters, n_noise)
         if n_clusters > 0:
             correctly_clustered, incorrectly_clustered, accuracy = compare_hr(trimmed_df, df_all, df_all_temp)
-    gaia_obj = GaiaData(hr_bytes, trimmed_bytes, distance_bytes, pm_bytes, correctly_clustered,
-                        incorrectly_clustered, accuracy, anomaly, actual_cluster_sizes)
+    gaia_obj = GaiaData(hr_bytes.getvalue(), trimmed_bytes.getvalue(), distance_bytes.getvalue(), pm_bytes.getvalue(), 
+                        correctly_clustered, incorrectly_clustered, accuracy, anomaly, actual_cluster_sizes)
+    distance_bytes.close()
+    hr_bytes.close()
+    trimmed_bytes.close()
+    pm_bytes.close()
     return gaia_obj
 
 def callback(ch, method, properties, body):
     request_info = json.loads(body)
+    csv_data = array.array('B', request_info["data_id"]).tobytes()
+    db_scan = request_info["db_scan"]
+    epsilon = request_info["epsilon"]
+    print(db_scan)
+    cluster_size = request_info["cluster_size"]
+    filename = request_info["filename"]
+    gaia_data = run_gaia(csv_data, db_scan, epsilon, cluster_size, filename)
+    ch.basic_ack(method.delivery_tag)
+    print("Done")
 
 def timeout_connect():
     start = time.time()
