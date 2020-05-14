2b19fcbf24435666d3bfc4c70ef668b64600d5c9
Zedjones <dojoman19@gmail.com>
2020-05-13 20:29:44 -0400

    Added initial model for computations

--- src/models.rs
+++ src/models.rs
@@ -7,3 +7,17 @@ struct Cluster {
     pub cluster_number: i32,
     pub stars_number: i32
 }
+
+#[derive(Queryable, Insertable)]
+#[table_name = "computations"]
+struct Computation {
+    pub id: i32,
+    pub hr_png: Vec<u8>,
+    pub trimmed_png: Vec<u8>,
+    pub distance_png: Vec<u8>,
+    pub pm_png: Option<Vec<u8>>,
+    pub correctly_clustered: Option<i32>,
+    pub incorrectly_clustered: Option<i32>,
+    pub accuracy: Option<f32>,
+    pub anomaly: Option<i32>
+}
